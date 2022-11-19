04002bca8309630d7bf1d50df29ca53e8e1d29de
carlos <cjr28rs@gmail.com>
2022-11-19 00:47:11 -0300

    er sa2 uc9

--- uc9/eplayers/js/script.js
+++ uc9/eplayers/js/script.js
@@ -1,3 +1,17 @@
 function cadastrarNewsletter(){
-    
+    let email = document.getElementById("campo-email").value
+    alert(email)
+}
+
+function mostrarmenu($event){
+    let menu = document.getElementById("menu")
+
+    if(menu.style.display == "none"){
+        menu.style.display = "flex"
+    }
+    else{
+        menu.style.display = "none"
+    }
+
+    event.preventDefault ();
 }
