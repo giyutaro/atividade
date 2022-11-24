4851679b048422e24ab5de77a21062302e27fd46
carlos <cjr28rs@gmail.com>
2022-11-24 02:22:49 -0300

    encontro remoto 4

--- uc9/eplayers/js/script.js
+++ uc9/eplayers/js/script.js
@@ -1,4 +1,41 @@
-$(document).ready
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-lol", "banner-cp", "banner-ds"]
+
+    setInterval(mudarSlide, 2000);
+
+    function mudarSlide(){
+        //remover o slider anterior
+        if (slideAtual > 0){
+            $("#carrossel").removeClass(listaSlides[slideAtual -1])
+        }
+        else{
+            $("#carrossel").removeClass(listaSlides[listaSlides.length -1])
+        }
+
+        //exibir o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+        //indicar qual o slide atual
+        console.log("Slide atual é: ",slideAtual)
+        slideAtual ++
+
+        if (slideAtual > listaSlides.length -1){
+            slideAtual = 0
+        }
+    }
+
+
+    $("#barras").click(function(){
+        if ($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+        }
+        else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
 
 
 function cadastrarNewsletter(){
