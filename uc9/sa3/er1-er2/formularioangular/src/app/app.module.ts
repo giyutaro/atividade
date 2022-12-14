0d964d5395318ebabd1b8f435c54a1c670d4a3a6
carlos <cjr28rs@gmail.com>
2022-12-14 16:13:04 -0300

    fazendo uc10

---
+++ uc9/sa3/er1-er2/formularioangular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { ContatoComponent } from './views/contato/contato.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
